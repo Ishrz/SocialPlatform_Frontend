830477555e655a7a1f09aefe04adf80fe9395ab2
Ishrz <ishrzz66@gmail.com>
2026-02-21 18:21:37 +0530

    integrated Backend api and login successfully

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import "./style.scss"
-import { Link } from 'react-router'
+import React, { useState } from "react";
+import "./style.scss";
+import { Link } from "react-router";
+import axios from "axios"
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler =async (e)=>{
+    e.preventDefault()    
+    console.log(email)
+    console.log(password)
+
+    const response=await axios.post("http://127.0.0.1:3000/v1/api/user/login",{
+        email:email,
+        password:password
+    })
+
+    console.log(response)
+
+
+    //clear input fileds after login
+    // setEmail("")
+    // setPassword("")
+  }
+
   return (
     <main>
-        <div className='form_container'>
-            <h1>Login</h1>
-            <form>
-                <input  placeholder='Email' type='text' / >
-                <input placeholder='Password' type='password' />
-                <button>Login</button>
-            </form>
-            <p>Dont have registerd account? <Link to="/register">Register</Link> </p>
-
-        </div>
+      <div className="form_container">
+        <h1>Login</h1>
+        <form onSubmit={submitHandler}>
+          <input value={email} onInput={(e)=> setEmail(e.target.value)} placeholder="Email" type="text" />
+          <input value={password} onInput={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
+          <button>Login</button>
+        </form>
+        <p>
+          Dont have registerd account? <Link to="/register">Register</Link>{" "}
+        </p>
+        
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
