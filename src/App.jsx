133e5f8f8d7e7d3197f08f2065bcee8e1e799a41
Ishrz <ishrzz66@gmail.com>
2026-03-08 07:12:28 +0530

    wrap comp with provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
+import { AuthProvider } from "./features/auth/auth.context"
 import AppRoutes from "./routes"
 const App = ( )=>{
 
   return (
     <>
-      
+      <AuthProvider>
       <AppRoutes/>
+      </AuthProvider>
     </>
   )
 }
--- src/features/auth/auth.context.jsx
+++ src/features/auth/auth.context.jsx
@@ -1,9 +1,10 @@
 import { createContext } from "react";
 import { registration, login, getMe } from "./services/auth.api";
 
-const authContext = createContext();
+const authContext = createContext(null);
 
 export function AuthProvider({ children }) {
+
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -11,7 +12,7 @@ export function AuthProvider({ children }) {
     setLoading(true)
     try {
       const response = await login(email, password);
-      setUser(response.data);
+      setUser(response);
       
     } catch (err) {
       console.log(err.message);
@@ -19,4 +20,23 @@ export function AuthProvider({ children }) {
         setLoading(false)
     }
   };
+
+  const handleRegister = async (username,password,email,bio,profilepic) =>{
+
+    setLoading(true)
+    try{
+      const response = await registration(username,password,email,bio,profilepic)
+      setUser(response)
+    }catch(err){
+      console.log(err.message)
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  return(
+    <authContext.Provider value={{user,loading,handleLogin,handleRegister}} >
+      {children}
+    </authContext.Provider>
+  )
 }
